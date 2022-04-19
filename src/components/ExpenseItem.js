8c0e22a920f8f398f66a19dc1a99b72816bf7586
Josh Hopkins <JHCZG1@hermanmiller.com>
2022-04-19 15:54:21 +0100

    Added components and a few other changes

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <div>
+        <ExpenseDate date={props.date} />
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
